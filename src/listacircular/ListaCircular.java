1d38849a6c37d31dbe80a5e79f25465689933be4
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-05 16:32:10 -0300

    ListaCircular: implementado get() e getNo()

--- src/listacircular/ListaCircular.java
+++ src/listacircular/ListaCircular.java
@@ -12,6 +12,27 @@ public class ListaCircular<T> {
 		this.tamanhoLista = 0;
 	}
 	
+	public T get(int index) {
+		return this.getNo(index).getConteudo();
+	}
+	
+	private No<T> getNo(int index) {
+		if (this.isEmpty())
+			throw new IndexOutOfBoundsException("A lista está vazia");
+		
+		if (index == 0) {
+			return this.cauda;
+		}
+		
+		No<T> noAuxiliar = this.cauda;
+		
+		for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+			noAuxiliar = noAuxiliar.getNoProximo();
+		}
+		
+		return noAuxiliar;
+	}
+	
 	public boolean isEmpty() {
 		return this.tamanhoLista == 0 ? true : false;
 	}

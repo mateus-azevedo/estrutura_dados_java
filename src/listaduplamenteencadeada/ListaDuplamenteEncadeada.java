fb92b3ed49d6acb99387312cb75314941dddd199
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-05 16:09:04 -0300

    ListaDuplamente: implementado add() + sobrecargas do metodo

--- src/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
+++ src/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -12,6 +12,47 @@ public class ListaDuplamenteEncadeada<T> {
 		this.tamanhoLista = 0;
 	}
 	
+	public void add(T elemento) {
+		NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+		
+		novoNo.setNoProximo(null);
+		novoNo.setNoPrevio(ultimoNo);
+		
+		if (primeiroNo == null) {
+			primeiroNo = novoNo;
+		}
+		
+		if (ultimoNo != null) {
+			ultimoNo.setNoProximo(novoNo);
+		}
+		
+		ultimoNo = novoNo;
+		tamanhoLista++;
+	}
+	
+	public void add(int index, T elemento) {
+		NoDuplo<T> noAuxiliar = getNo(index);
+		NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+		
+		novoNo.setNoProximo(noAuxiliar);
+		
+		if (novoNo.getNoProximo() != null) {
+			novoNo.setNoPrevio(noAuxiliar.getNoPrevio());
+			novoNo.getNoProximo().setNoPrevio(novoNo);
+		} else {
+			novoNo.setNoPrevio(ultimoNo);
+			ultimoNo = novoNo;
+		}
+		
+		if (index == 0) {
+			primeiroNo = novoNo;
+		} else {
+			novoNo.getNoPrevio().setNoProximo(novoNo);
+		}
+		
+		tamanhoLista++;
+	}
+	
 	public T get(int index) {
 		return this.getNo(index).getConteudo();
 	}
